c1b410544984e0522297e372e5640b4b54c748d9
ASHRAF <ashirafkagga@gmail.com>
2024-02-27 18:10:19 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/componets/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  );
+}
---
+++ componets/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'next/image';
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
